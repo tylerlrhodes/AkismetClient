5bdd7105cfe68b71f2afb011c3eadb136fc12422
Tyler Rhodes <tylerlrhodes@gmail.com>
2017-12-10 21:10:46 -0500

    updates

--- TylerRhodes.Akismet.Test/Tests.cs
+++ TylerRhodes.Akismet.Test/Tests.cs
@@ -7,71 +7,66 @@ using System.Threading.Tasks;
 
 namespace TylerRhodes.Akismet.Test
 {
-    public class Tests
+  public class Tests
+  {
+    private readonly string _apiKey;
+    private readonly HttpClient _http;
+
+    public Tests()
     {
-        private string _apiKey;
+      var builder = new ConfigurationBuilder();
 
-        public Tests()
-        {
-            var builder = new ConfigurationBuilder();
+      builder.AddUserSecrets<Tests>();
 
-            builder.AddUserSecrets<Tests>();
+      var config = builder.Build();
 
-            var config = builder.Build();
+      _apiKey = config["ApiKey"];
 
-            _apiKey = config["ApiKey"];
-        }
-
-        [Fact]
-        public async Task CommentIsNotSpamAsync()
-        {
-            var comment = new AkismetComment()
-            {
-                UserIp = "127.0.0.1",
-                UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-                Blog = "http://www.bagombo.org",
-                Author = "Tyler Rhodes",
-                AuthorEmail = "tylerlrhodes@gmail.com",
-                CommentType = "comment",
-                Content = "This is a test that should not be marked as spam due to the author value."
-            };
-
-            bool isSpam;
-
-            using (var http = new HttpClient())
-            {
-                var akismetClient = new AkismetClient(comment.Blog, _apiKey, http);
-
-                isSpam = await akismetClient.IsCommentSpam(comment);
-            }
-
-            Assert.False(isSpam);
-        }
-
-        [Fact]
-        public async Task CommentIsSpamAsync()
-        {
-            var comment = new AkismetComment()
-            {
-                UserIp = "127.0.0.1",
-                UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-                Blog = "http://www.bagombo.org",
-                Author = "viagra-test-123",
-                AuthorEmail = "test@test.com",
-                CommentType = "comment",
-                Content = "This is a test that should be marked as spam due to the author value."
-            };
-
-            bool isSpam;
-
-            using (var http = new HttpClient())
-            {
-                var akismetClient = new AkismetClient(comment.Blog, _apiKey, http);
-
-                isSpam = await akismetClient.IsCommentSpam(comment);
-            }
-
-            Assert.True(isSpam);
-        }
+      _http = new HttpClient();
     }
+
+    [Fact]
+    public async Task CommentIsNotSpamAsync()
+    {
+      var comment = new AkismetComment()
+      {
+        UserIp = "127.0.0.1",
+        UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        Blog = "http://www.bagombo.org",
+        Referrer = "http://www.google.com",
+        Permalink = "http://www.bagombo.org/not-spam",
+        Author = "Tyler Rhodes",
+        AuthorEmail = "tylerlrhodes@gmail.com",
+        CommentType = "comment",
+        Content = "This is a test that should not be marked as spam due to the author value.  Sometimes it seems to come back as spam... Not sure why"
+      };
+
+      var akismetClient = new AkismetClient(comment.Blog, _apiKey, _http);
+
+      var isSpam = await akismetClient.IsCommentSpam(comment);
+
+      Assert.False(isSpam);
+    }
+
+    [Fact]
+    public async Task CommentIsSpamAsync()
+    {
+      var comment = new AkismetComment()
+      {
+        UserIp = "127.0.0.1",
+        UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        Blog = "http://www.bagombo.org",
+        Author = "viagra-test-123",
+        AuthorEmail = "test@test.com",
+        CommentType = "comment",
+        Content = "This is a test that should be marked as spam due to the author value."
+      };
+
+      var akismetClient = new AkismetClient(comment.Blog, _apiKey, _http);
+
+      var isSpam = await akismetClient.IsCommentSpam(comment);
+      
+      Assert.True(isSpam);
+    }
+  }
 }
