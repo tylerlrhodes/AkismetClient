58787e237471b604ec8b5a0870b1ab0e64c9e33b
Tyler Rhodes <tylerlrhodes@gmail.com>
2017-12-11 20:48:30 -0500

    resharper fixups

--- TylerRhodes.Akismet/AkismetClient.cs
+++ TylerRhodes.Akismet/AkismetClient.cs
@@ -8,78 +8,103 @@ using System.Web;
 namespace TylerRhodes.Akismet
 {
   /// <summary>
-  /// Akismet client for interfacing with Akismet public anti-spam API over http
+  ///   Akismet client for interfacing with Akismet public anti-spam API over http
   /// </summary>
   public class AkismetClient
   {
     /// <summary>
-    /// HttpClient to use, submitted through constructor
+    ///   Akismet Key Verification url
+    /// </summary>
+    private const string KeyVerificationUrl = "https://rest.akismet.com/1.1/verify-key";
+
+    /// <summary>
+    ///   Akismet comment check url format string
+    /// </summary>
+    private const string CommentCheckUrlFormat = "https://{0}.rest.akismet.com/1.1/comment-check";
+
+    /// <summary>
+    ///   Akismet Submit Spam url format string
+    /// </summary>
+    private const string SubmitSpamUrlFormat = "https://{0}.rest.akismet.com/1.1/submit-spam";
+
+    /// <summary>
+    ///   Akismet Submit Ham url format string
+    /// </summary>
+    private const string SubmitHamUrlFormat = "https://{0}.rest.akismet.com/1.1/submit-ham";
+
+    /// <summary>
+    ///   HttpClient to use, submitted through constructor
     /// </summary>
     private readonly HttpClient _httpClient;
 
     /// <summary>
-    /// Akismet Key Verification url
-    /// </summary>
-    private const string KeyVerificationUrl = "https://rest.akismet.com/1.1/verify-key";
-    /// <summary>
-    /// Akismet key verification uri
+    ///   Akismet key verification uri
     /// </summary>
     private readonly Uri _keyVerificationUri = new Uri(KeyVerificationUrl);
 
     /// <summary>
-    /// Akismet comment check url format string
-    /// </summary>
-    private const string CommentCheckUrlFormat = "https://{0}.rest.akismet.com/1.1/comment-check";
-    /// <summary>
-    /// Akistmet comment check uri
-    /// </summary>
-    private Uri _commentCheckUri;
-
-    /// <summary>
-    /// Akismet Submit Spam url format string
-    /// </summary>
-    private const string SubmitSpamUrlFormat = "https://{0}.rest.akismet.com/1.1/submit-spam";
-    /// <summary>
-    /// Akismet submit spam uri
-    /// </summary>
-    private Uri _submitSpamUri;
-
-    /// <summary>
-    /// Akismet Submit Ham url format string
-    /// </summary>
-    private const string SubmitHamUrlFormat = "https://{0}.rest.akismet.com/1.1/submit-ham";
-    /// <summary>
-    /// Akismet submit ham uri
-    /// </summary>
-    private Uri _submitHamUri;
-
-    /// <summary>
-    /// blog url
-    /// </summary>
-    public readonly string BlogUrl;
-    /// <summary>
-    /// api key
+    ///   api key
     /// </summary>
     public readonly string ApiKey;
 
     /// <summary>
-    /// AkismetClient constructor
+    ///   blog url
     /// </summary>
-    /// <param name="blogUrl">URL of blog</param>
+    public readonly string BlogUrl;
+
+    /// <summary>
+    ///   Akistmet comment check uri
+    /// </summary>
+    private Uri _commentCheckUri;
+
+    /// <summary>
+    ///   Akismet submit ham uri
+    /// </summary>
+    private Uri _submitHamUri;
+
+    /// <summary>
+    ///   Akismet submit spam uri
+    /// </summary>
+    private Uri _submitSpamUri;
+
+    /// <summary>
+    ///   Option to throw an exception on a non 2xx http response
+    /// </summary>
+    private readonly bool _throwOnInvalidHttpStatus;
+
+    /// <inheritdoc />
+    /// <summary>
+    ///   AkismetClient constructor
+    /// </summary>
+    /// <param name="blogUrl">url of blog</param>
     /// <param name="apiKey">Akismet API Key</param>
     /// <param name="httpClient">HttpClient to use</param>
     public AkismetClient(string blogUrl, string apiKey, HttpClient httpClient)
+      : this(blogUrl, apiKey, httpClient, false)
+    {
+    }
+
+    /// <summary>
+    ///   Akismet Constructor with option to throw on non 2xx http response
+    /// </summary>
+    /// <param name="blogUrl">url of blog</param>
+    /// <param name="apiKey">Akismet API Key</param>
+    /// <param name="httpClient">HttpClient to use</param>
+    /// <param name="throwOnInvalidHttpStatus">Whether to throw an exception on non 2xx http responses</param>
+    public AkismetClient(string blogUrl, string apiKey, HttpClient httpClient, bool throwOnInvalidHttpStatus)
     {
       BlogUrl = blogUrl ?? throw new ArgumentNullException(nameof(blogUrl));
       ApiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
 
       _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
 
+      _throwOnInvalidHttpStatus = throwOnInvalidHttpStatus;
+
       GenerateUris();
     }
 
     /// <summary>
-    /// Generates URIs for use by AkismetClient
+    ///   Generates URIs for use by AkismetClient
     /// </summary>
     private void GenerateUris()
     {
@@ -89,20 +114,23 @@ namespace TylerRhodes.Akismet
     }
 
     /// <summary>
-    /// Verifies API Key
+    ///   Verifies API Key
     /// </summary>
     /// <returns>true for valid key</returns>
     public async Task<bool> CheckApiKey()
     {
       var parameters = new Dictionary<string, string>
       {
-        { "key", HttpUtility.UrlEncode(ApiKey) },
-        { "blog", HttpUtility.UrlEncode(BlogUrl) }
+        {"key", HttpUtility.UrlEncode(ApiKey)},
+        {"blog", HttpUtility.UrlEncode(BlogUrl)}
       };
 
       var content = new FormUrlEncodedContent(parameters);
 
       var httpResponseMessage = await _httpClient.PostAsync(_keyVerificationUri, content).ConfigureAwait(false);
+
+      if (_throwOnInvalidHttpStatus)
+        httpResponseMessage.EnsureSuccessStatusCode();
 
       var result = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
 
@@ -110,7 +138,7 @@ namespace TylerRhodes.Akismet
     }
 
     /// <summary>
-    /// Submits a comment as spam to Akismet
+    ///   Submits a comment as spam to Akismet
     /// </summary>
     /// <param name="comment">Spam comment to submit</param>
     /// <returns></returns>
@@ -120,7 +148,7 @@ namespace TylerRhodes.Akismet
     }
 
     /// <summary>
-    /// Submits a comment as ham to Akismet
+    ///   Submits a comment as ham to Akismet
     /// </summary>
     /// <param name="comment">Comment to submit as ham</param>
     /// <returns></returns>
@@ -130,7 +158,7 @@ namespace TylerRhodes.Akismet
     }
 
     /// <summary>
-    /// Checks a comment against Akismet to see if it is spam
+    ///   Checks a comment against Akismet to see if it is spam
     /// </summary>
     /// <param name="comment">Comment to check</param>
     /// <returns>true if comment is spam</returns>
@@ -140,7 +168,7 @@ namespace TylerRhodes.Akismet
     }
 
     /// <summary>
-    /// Submits a comment to the given URI
+    ///   Submits a comment to the given URI
     /// </summary>
     /// <param name="comment">Comment to submit</param>
     /// <param name="uri">URI to submit to</param>
@@ -165,29 +193,32 @@ namespace TylerRhodes.Akismet
       var user_role = comment.UserRole ?? "";
       var is_test = comment.IsTest ?? "";
 
-      var parameters = new Dictionary<string, string>()
+      var parameters = new Dictionary<string, string>
       {
-        { "blog", HttpUtility.UrlEncode(blog) },
-        { "user_ip", HttpUtility.UrlEncode(user_ip) },
-        { "user_agent", HttpUtility.UrlEncode(user_agent) },
-        { "referrer", HttpUtility.UrlEncode(referrer) },
-        { "permalink", HttpUtility.UrlEncode(permalink) },
-        { "comment_type", HttpUtility.UrlEncode(comment_type) },
-        { "comment_author", HttpUtility.UrlEncode(comment_author) },
-        { "comment_author_email", HttpUtility.UrlEncode(comment_author_email) },
-        { "comment_author_url", HttpUtility.UrlEncode(comment_author_url) },
-        { "comment_content", HttpUtility.UrlEncode(comment_content) },
-        { "comment_date_gmt", HttpUtility.UrlEncode(comment_date_gmt) },
-        { "comment_post_modified_gmt", HttpUtility.UrlEncode(comment_post_modified_gmt) },
-        { "blog_lang", HttpUtility.UrlEncode(blog_lang) },
-        { "blog_charset", HttpUtility.UrlEncode(blog_charset) },
-        { "user_role", HttpUtility.UrlEncode(user_role) },
-        { "is_test", HttpUtility.UrlEncode(is_test) }
+        {"blog", HttpUtility.UrlEncode(blog)},
+        {"user_ip", HttpUtility.UrlEncode(user_ip)},
+        {"user_agent", HttpUtility.UrlEncode(user_agent)},
+        {"referrer", HttpUtility.UrlEncode(referrer)},
+        {"permalink", HttpUtility.UrlEncode(permalink)},
+        {"comment_type", HttpUtility.UrlEncode(comment_type)},
+        {"comment_author", HttpUtility.UrlEncode(comment_author)},
+        {"comment_author_email", HttpUtility.UrlEncode(comment_author_email)},
+        {"comment_author_url", HttpUtility.UrlEncode(comment_author_url)},
+        {"comment_content", HttpUtility.UrlEncode(comment_content)},
+        {"comment_date_gmt", HttpUtility.UrlEncode(comment_date_gmt)},
+        {"comment_post_modified_gmt", HttpUtility.UrlEncode(comment_post_modified_gmt)},
+        {"blog_lang", HttpUtility.UrlEncode(blog_lang)},
+        {"blog_charset", HttpUtility.UrlEncode(blog_charset)},
+        {"user_role", HttpUtility.UrlEncode(user_role)},
+        {"is_test", HttpUtility.UrlEncode(is_test)}
       };
 
       var content = new FormUrlEncodedContent(parameters);
 
       var httpResponseMessage = await _httpClient.PostAsync(uri, content).ConfigureAwait(false);
+
+      if (_throwOnInvalidHttpStatus)
+        httpResponseMessage.EnsureSuccessStatusCode();
 
       var result = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
 
